5be59bb947b4e7d3876432c16eb65d56c8de68d7
SergGrebnev <serggrebnev79@gmail.com>
2024-06-03 00:27:24 +0700

    Add files.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusService bonus = new BonusService();
+        System.out.println(bonus.calculate(500, true));
+    }
+}
